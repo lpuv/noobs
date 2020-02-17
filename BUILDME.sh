9b56fb35fc29d1502e6a0ebd662b21dc54884ea9
Restyled.io <commits@restyled.io>
2020-02-17 01:13:51 +0000

    Restyled by shfmt

--- BUILDME.sh
+++ BUILDME.sh
@@ -7,8 +7,7 @@ set -e
 # Final directory where NOOBS files will be copied to
 NOOBS_OUTPUT_DIR="output"
 
-
-function get_package_version {
+function get_package_version() {
   PACKAGE=$1
   CONFIG_FILE="package/$PACKAGE/$PACKAGE.mk"
   if [ -f "$CONFIG_FILE" ]; then
@@ -17,84 +16,79 @@ function get_package_version {
   fi
 }
 
-
-function update_github_package_version {
-    PACKAGE=$1
-    GITHUB_REPO=$2
-    BRANCH=$3
-    CONFIG_FILE="package/$PACKAGE/$PACKAGE.mk"
-    if [ -f "$CONFIG_FILE" ]; then
-        OLDREV=$(get_package_version "$PACKAGE")
-        if [ -z "$OLDREV" ]; then
-            echo "Error getting OLDREV for $PACKAGE";
-        else
-            REPO_API=https://api.github.com/repos/$GITHUB_REPO/git/refs/heads/$BRANCH
-            GITREV=$(curl -s "$REPO_API" | awk '{ if ($1 == "\"sha\":") { print substr($2, 2, 40) } }')
-            if [ -z "$GITREV" ]; then
-                echo "Error getting GITREV for $PACKAGE ($BRANCH)";
-            else
-                if [ "$OLDREV" == "$GITREV" ]; then
-                    echo "Package $PACKAGE ($BRANCH) is already newest version"
-                else
-                    CONFIG_VAR=$(echo "$PACKAGE-version" | tr '[:lower:]-' '[:upper:]_')
-                    sed -ri "s/(^$CONFIG_VAR\s*=\s*)[0-9a-f]+$/\1$GITREV/" "$CONFIG_FILE"
-                    echo "Package $PACKAGE ($BRANCH) updated to version $GITREV"
-                fi
-            fi
-        fi
+function update_github_package_version() {
+  PACKAGE=$1
+  GITHUB_REPO=$2
+  BRANCH=$3
+  CONFIG_FILE="package/$PACKAGE/$PACKAGE.mk"
+  if [ -f "$CONFIG_FILE" ]; then
+    OLDREV=$(get_package_version "$PACKAGE")
+    if [ -z "$OLDREV" ]; then
+      echo "Error getting OLDREV for $PACKAGE"
     else
-        echo "$CONFIG_FILE doesn't exist"
-    fi
-}
-
-
-function get_kernel_version {
-    CONFIG_FILE=.config
-    CONFIG_VAR=BR2_LINUX_KERNEL_VERSION
-    grep -E "^$CONFIG_VAR=\".+\"$" "$CONFIG_FILE" | tr -d '"' | cut -d= -f2
-}
-
-
-function update_github_kernel_version {
-    PACKAGE=kernel
-    GITHUB_REPO=$1
-    BRANCH=$2
-    CONFIG_FILE=.config
-    if [ -f "$CONFIG_FILE" ]; then
-        OLDREV=$(get_kernel_version)
-        if [ -z "$OLDREV" ]; then
-            echo "Error getting OLDREV for $PACKAGE";
+      REPO_API=https://api.github.com/repos/$GITHUB_REPO/git/refs/heads/$BRANCH
+      GITREV=$(curl -s "$REPO_API" | awk '{ if ($1 == "\"sha\":") { print substr($2, 2, 40) } }')
+      if [ -z "$GITREV" ]; then
+        echo "Error getting GITREV for $PACKAGE ($BRANCH)"
+      else
+        if [ "$OLDREV" == "$GITREV" ]; then
+          echo "Package $PACKAGE ($BRANCH) is already newest version"
         else
-            REPO_API=https://api.github.com/repos/$GITHUB_REPO/git/refs/heads/$BRANCH
-            GITREV=$(curl -s "$REPO_API" | awk '{ if ($1 == "\"sha\":") { print substr($2, 2, 40) } }')
-            if [ -z "$GITREV" ]; then
-                echo "Error getting GITREV for $PACKAGE ($BRANCH)";
-            else
-                if [ "$OLDREV" == "$GITREV" ]; then
-                    echo "$PACKAGE ($BRANCH) is already newest version"
-                else
-                    CONFIG_VAR=BR2_LINUX_KERNEL_CUSTOM_REPO_VERSION
-                    sed -ri "s/(^$CONFIG_VAR=\")[0-9a-f]+(\")$/\1$GITREV\2/" "$CONFIG_FILE"
-                    CONFIG_VAR=BR2_LINUX_KERNEL_VERSION
-                    sed -ri "s/(^$CONFIG_VAR=\")[0-9a-f]+(\")$/\1$GITREV\2/" "$CONFIG_FILE"
-                    echo "$PACKAGE ($BRANCH) updated to version $GITREV"
-                fi
-            fi
+          CONFIG_VAR=$(echo "$PACKAGE-version" | tr '[:lower:]-' '[:upper:]_')
+          sed -ri "s/(^$CONFIG_VAR\s*=\s*)[0-9a-f]+$/\1$GITREV/" "$CONFIG_FILE"
+          echo "Package $PACKAGE ($BRANCH) updated to version $GITREV"
         fi
-    else
-        echo "$CONFIG_FILE doesn't exist"
+      fi
     fi
+  else
+    echo "$CONFIG_FILE doesn't exist"
+  fi
 }
 
-
-function select_kernelconfig {
-    ARCH=$1
-    CONFIG_FILE=.config
-    CONFIG_VAR=BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE
-    VAR_PREFIX=kernelconfig-recovery
-    sed -ri "s/(^$CONFIG_VAR=\"$VAR_PREFIX\.).+(\")$/\1$ARCH\2/" "$CONFIG_FILE"
+function get_kernel_version() {
+  CONFIG_FILE=.config
+  CONFIG_VAR=BR2_LINUX_KERNEL_VERSION
+  grep -E "^$CONFIG_VAR=\".+\"$" "$CONFIG_FILE" | tr -d '"' | cut -d= -f2
 }
 
+function update_github_kernel_version() {
+  PACKAGE=kernel
+  GITHUB_REPO=$1
+  BRANCH=$2
+  CONFIG_FILE=.config
+  if [ -f "$CONFIG_FILE" ]; then
+    OLDREV=$(get_kernel_version)
+    if [ -z "$OLDREV" ]; then
+      echo "Error getting OLDREV for $PACKAGE"
+    else
+      REPO_API=https://api.github.com/repos/$GITHUB_REPO/git/refs/heads/$BRANCH
+      GITREV=$(curl -s "$REPO_API" | awk '{ if ($1 == "\"sha\":") { print substr($2, 2, 40) } }')
+      if [ -z "$GITREV" ]; then
+        echo "Error getting GITREV for $PACKAGE ($BRANCH)"
+      else
+        if [ "$OLDREV" == "$GITREV" ]; then
+          echo "$PACKAGE ($BRANCH) is already newest version"
+        else
+          CONFIG_VAR=BR2_LINUX_KERNEL_CUSTOM_REPO_VERSION
+          sed -ri "s/(^$CONFIG_VAR=\")[0-9a-f]+(\")$/\1$GITREV\2/" "$CONFIG_FILE"
+          CONFIG_VAR=BR2_LINUX_KERNEL_VERSION
+          sed -ri "s/(^$CONFIG_VAR=\")[0-9a-f]+(\")$/\1$GITREV\2/" "$CONFIG_FILE"
+          echo "$PACKAGE ($BRANCH) updated to version $GITREV"
+        fi
+      fi
+    fi
+  else
+    echo "$CONFIG_FILE doesn't exist"
+  fi
+}
+
+function select_kernelconfig() {
+  ARCH=$1
+  CONFIG_FILE=.config
+  CONFIG_VAR=BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE
+  VAR_PREFIX=kernelconfig-recovery
+  sed -ri "s/(^$CONFIG_VAR=\"$VAR_PREFIX\.).+(\")$/\1$ARCH\2/" "$CONFIG_FILE"
+}
 
 export QT_SELECT=4
 
@@ -106,36 +100,36 @@ IMAGES_DIR="output/images"
 
 # Delete buildroot build directory to force rebuild
 if [ -e "$BUILD_DIR" ]; then
-    rm -rf "$BUILD_DIR/recovery-$(get_package_version recovery)" || true
+  rm -rf "$BUILD_DIR/recovery-$(get_package_version recovery)" || true
 fi
 
 SKIP_KERNEL_REBUILD=0
 
 for i in "$@"; do
-    # Update raspberrypi/firmware master HEAD version in package/rpi-firmware/rpi-firmware.mk to latest
-    if [ "$i" = "update-firmware" ]; then
-        update_github_package_version rpi-firmware raspberrypi/firmware stable
-    fi
+  # Update raspberrypi/firmware master HEAD version in package/rpi-firmware/rpi-firmware.mk to latest
+  if [ "$i" = "update-firmware" ]; then
+    update_github_package_version rpi-firmware raspberrypi/firmware stable
+  fi
 
-    # Update raspberrypi/userland master HEAD version in package/rpi-userland/rpi-userland.mk to latest
-    if [ "$i" = "update-userland" ]; then
-        update_github_package_version rpi-userland raspberrypi/userland master
-    fi
+  # Update raspberrypi/userland master HEAD version in package/rpi-userland/rpi-userland.mk to latest
+  if [ "$i" = "update-userland" ]; then
+    update_github_package_version rpi-userland raspberrypi/userland master
+  fi
 
-    # Update raspberrypi/linux rpi-4.1.y HEAD version in buildroot/.config to latest
-    if [ "$i" = "update-kernel" ]; then
-        update_github_kernel_version raspberrypi/linux rpi-4.14.y
-    fi
+  # Update raspberrypi/linux rpi-4.1.y HEAD version in buildroot/.config to latest
+  if [ "$i" = "update-kernel" ]; then
+    update_github_kernel_version raspberrypi/linux rpi-4.14.y
+  fi
 
-    # Option to build just recovery without completely rebuilding both kernels
-    if [ "$i" = "skip-kernel-rebuild" ]; then
-        SKIP_KERNEL_REBUILD=1
-    fi
+  # Option to build just recovery without completely rebuilding both kernels
+  if [ "$i" = "skip-kernel-rebuild" ]; then
+    SKIP_KERNEL_REBUILD=1
+  fi
 
-    # Early-exit (in case we want to just update config files without doing a build)
-    if [ "$i" = "nobuild" ]; then
-        exit
-    fi
+  # Early-exit (in case we want to just update config files without doing a build)
+  if [ "$i" = "nobuild" ]; then
+    exit
+  fi
 done
 
 # Let buildroot build everything
@@ -148,25 +142,25 @@ mkdir -p "$FINAL_OUTPUT_DIR/os"
 cp -r ../sdcontent/* "$FINAL_OUTPUT_DIR"
 
 if [ "$SKIP_KERNEL_REBUILD" -ne 1 ]; then
-    # Rebuild kernel for ARMv7l
-    select_kernelconfig armv7l
-    make linux-reconfigure
-    # copy ARMv7l kernel
-    cp "$IMAGES_DIR/zImage" "$FINAL_OUTPUT_DIR/recovery7l.img"
+  # Rebuild kernel for ARMv7l
+  select_kernelconfig armv7l
+  make linux-reconfigure
+  # copy ARMv7l kernel
+  cp "$IMAGES_DIR/zImage" "$FINAL_OUTPUT_DIR/recovery7l.img"
 
-    # Rebuild kernel for ARMv7
-    select_kernelconfig armv7
-    make linux-reconfigure
-    # copy ARMv7 kernel
-    cp "$IMAGES_DIR/zImage" "$FINAL_OUTPUT_DIR/recovery7.img"
+  # Rebuild kernel for ARMv7
+  select_kernelconfig armv7
+  make linux-reconfigure
+  # copy ARMv7 kernel
+  cp "$IMAGES_DIR/zImage" "$FINAL_OUTPUT_DIR/recovery7.img"
 
-    # Rebuild kernel for ARMv6
-    select_kernelconfig armv6
-    make linux-reconfigure
-    # copy ARMv6 kernel
-    cp "$IMAGES_DIR/zImage" "$FINAL_OUTPUT_DIR/recovery.img"
+  # Rebuild kernel for ARMv6
+  select_kernelconfig armv6
+  make linux-reconfigure
+  # copy ARMv6 kernel
+  cp "$IMAGES_DIR/zImage" "$FINAL_OUTPUT_DIR/recovery.img"
 else
-    echo "Warning: kernels in '$NOOBS_OUTPUT_DIR' directory haven't been updated"
+  echo "Warning: kernels in '$NOOBS_OUTPUT_DIR' directory haven't been updated"
 fi
 
 # copy rootfs
@@ -183,12 +177,12 @@ touch "$FINAL_OUTPUT_DIR/RECOVERY_FILES_DO_NOT_EDIT"
 
 # Create build-date timestamp file containing Git HEAD info for build
 BUILD_INFO="$FINAL_OUTPUT_DIR/BUILD-DATA"
-echo "Build-date: $(date +"%Y-%m-%d")" > "$BUILD_INFO"
-echo "NOOBS Version: $(sed -n 's|.*VERSION_NUMBER.*\"\(.*\)\"|v\1|p' ../recovery/config.h)" >> "$BUILD_INFO"
-echo "NOOBS Git HEAD @ $(git rev-parse --verify HEAD)" >> "$BUILD_INFO"
-echo "rpi-userland Git master @ $(get_package_version rpi-userland)" >> "$BUILD_INFO"
-echo "rpi-firmware Git master @ $(get_package_version rpi-firmware)" >> "$BUILD_INFO"
-echo "rpi-linux Git rpi-4.14.y @ $(get_kernel_version)" >> "$BUILD_INFO"
+echo "Build-date: $(date +"%Y-%m-%d")" >"$BUILD_INFO"
+echo "NOOBS Version: $(sed -n 's|.*VERSION_NUMBER.*\"\(.*\)\"|v\1|p' ../recovery/config.h)" >>"$BUILD_INFO"
+echo "NOOBS Git HEAD @ $(git rev-parse --verify HEAD)" >>"$BUILD_INFO"
+echo "rpi-userland Git master @ $(get_package_version rpi-userland)" >>"$BUILD_INFO"
+echo "rpi-firmware Git master @ $(get_package_version rpi-firmware)" >>"$BUILD_INFO"
+echo "rpi-linux Git rpi-4.14.y @ $(get_kernel_version)" >>"$BUILD_INFO"
 
 cd ..
 
